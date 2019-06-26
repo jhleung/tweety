045de9a67d112e99d3bd743bae40968340daf30c
= <jeffrey.leung@live.com>
2019-06-26 10:05:24 -0500

    add url mapping/regex to cors

--- src/main/java/com/jeff/TweetyApplication.java
+++ src/main/java/com/jeff/TweetyApplication.java
@@ -7,7 +7,9 @@ import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.servlet.DispatcherType;
 import javax.servlet.FilterRegistration;
+import java.util.EnumSet;
 
 public class TweetyApplication extends Application<TweetyConfiguration> {
     private static final Logger logger = LoggerFactory.getLogger(TweetyApplication.class);
@@ -24,13 +26,14 @@ public class TweetyApplication extends Application<TweetyConfiguration> {
                 .tweetyModule(twitter)
                 .build();
         TweetyResource tweetyResource = component.getTweetyResource();
+        environment.jersey().register(tweetyResource);
 
         final FilterRegistration.Dynamic cors = environment.servlets().addFilter("CORS", CrossOriginFilter.class);
         cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
         cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin,Authorization");
-        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "OPTIONS,GET,PUT,POST,DELETE,HEAD");
+        cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,");
+        cors.addMappingForUrlPatterns(EnumSet.allOf(DispatcherType.class), true, "/*");
 
-        environment.jersey().register(tweetyResource);
         logger.info("Tweety application started successfully.");
     }
 }
